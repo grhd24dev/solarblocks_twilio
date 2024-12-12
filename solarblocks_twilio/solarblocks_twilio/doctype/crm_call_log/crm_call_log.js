4066fec3bdac8a2e3fae1d140961d8659696ef09
grhd24dev <grhd24dev@gmail.com>
2024-12-12 22:27:27 +0530

    recording btn

--- solarblocks_twilio/solarblocks_twilio/doctype/crm_call_log/crm_call_log.js
+++ solarblocks_twilio/solarblocks_twilio/doctype/crm_call_log/crm_call_log.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CRM Call Log", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("CRM Call Log", {
+	listen_recording: (frm) => {
+		window.open(frm.doc.recording_url);
+	}
+}
+);
